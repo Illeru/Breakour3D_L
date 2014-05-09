988a1dbdc19a0ab29ff69bff392d7aa3a52fca31
Illeru <leonvanriesen@hotmail.com>
2014-05-09 12:16:47 +0200

    New Breakour3D Version 0.2.1  -New GunRotation Script  -New Gun and Spawn Object

---
+++ Breakout3Dv0.2.1/Assets/Scripts/Gun.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gun : MonoBehaviour {
+
+	public int mouseSensivity;
+	static int rotationFix = 0;
+
+	// Use this for initialization
+	void Start () 
+	{
+		Screen.lockCursor = true;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+		//Rotation
+		//float maxRot = 60f;
+		float rotLeftRight = 0f;
+		float rotUpDown = 0f;
+
+		rotLeftRight = Input.GetAxis ("Mouse X") * mouseSensivity;
+	    rotUpDown = Input.GetAxis ("Mouse Y") * mouseSensivity;
+
+		transform.Rotate(rotationFix, 0, rotLeftRight);
+		transform.Rotate (rotUpDown, 0,rotationFix);
+
+
+
+	}
+}
